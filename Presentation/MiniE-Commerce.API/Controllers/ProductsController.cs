aa89df90296030e24b2c541c655025460a99dcfc
Azima <azimaqadirli@gmail.com>
2025-01-25 23:30:34 +0400

    TrackingAdded

--- Presentation/MiniE-Commerce.API/Controllers/ProductsController.cs
+++ Presentation/MiniE-Commerce.API/Controllers/ProductsController.cs
@@ -19,12 +19,15 @@ namespace MiniE_Commerce.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 10, CreatedAt = DateTime.Now, Stock = 100},
-                new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 20, CreatedAt = DateTime.Now, Stock = 200},
-                new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 30, CreatedAt = DateTime.Now, Stock = 300},
-            });
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 10, CreatedAt = DateTime.Now, Stock = 100},
+            //    new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 20, CreatedAt = DateTime.Now, Stock = 200},
+            //    new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 30, CreatedAt = DateTime.Now, Stock = 300},
+            //});
+            //var count = await _productWriteRepository.SaveAsync();
+            Product p = await _productReadRepository.GetByIdAsync("F5B633E3-F118-4053-B86F-DA413D9D24F7", true);
+            p.Name = "Mercedes CLE200";
             await _productWriteRepository.SaveAsync();
         }
         [HttpGet("id")]
