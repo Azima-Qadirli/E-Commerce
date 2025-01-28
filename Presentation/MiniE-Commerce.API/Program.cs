c493633810aa15006d906662cedcd9bb98026c3a
Azima <azimaqadirli@gmail.com>
2025-01-29 00:37:45 +0400

    CORS added

--- Presentation/MiniE-Commerce.API/Program.cs
+++ Presentation/MiniE-Commerce.API/Program.cs
@@ -2,6 +2,10 @@ using MiniE_Commerce.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+));
+
 builder.Services.AddPersistence();
 builder.Services.AddControllers();
 
@@ -10,13 +14,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
